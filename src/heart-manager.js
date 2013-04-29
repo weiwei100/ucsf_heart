5dac3d2a96ee077f0857cd0496be2911adab1a65
Jonathan Palley <jpalley@gmail.com>
2013-04-29 20:08:43 +0800

    more savng updates

--- src/heart-manager.js
+++ src/heart-manager.js
@@ -30,32 +30,40 @@ var HEART = {
 		});
 	},
 	
-	toSensor: function(content, succ, fail) {
-//		var url = 'http://sensocol.bj.brainpage.com/v1/sensors/'+HEART.uuid;
-		var url = 'http://localhost:8881/v1/sensors/' + HEART.uuid;
+	toSensocol: function(content, succ, fail){
+		var url = 'http://sensocol.bj.brainpage.com/v1/sensors/events'; //+HEART.uuid;
+		//var url = 'http://localhost:8881/v1/sensors/' + HEART.uuid;
 		var username = '2551135934';
 		var password = '4bbdbcd0b64e75ca047aac5e7d44934b';
 		var base = Base64.encode(username + ':' + password);
 		
 		var auth = "Basic " + base;
+		HEART.request(url, auth, content, succ, fail);
+
+		
+	},
+	toSensor: function(content) {
+
 		
 		uuid = HEART.uuid;
 		content.user = uuid;
 		succ = function(response) {
 			console.log(response);
+			HEART.sync();
 		};
 
 		fail = function(response) {
 			//Ext.Msg.alert(';(', "<div align='center'>Network Error</div>", Ext.emptyFn);
 			feeds = Ext.getStore("Feeds");
-			content.timestamp = Date.now();
-			feeds.add(content);
+			store = {};
+			store.data = content;
+			store.timestamp = Date.now();
+			feeds.add(store);
 			feeds.sort("timestamp", 'DESC');		
 			feeds.sync();
 		};
-
-		HEART.request(url, auth, content, succ, fail);
-		//HEART.sync();
+		HEART.toSensocol(content, succ, fail);
+		
 	
 	},
 	
@@ -79,12 +87,13 @@ var HEART = {
 			feeds.each(
 				function(item, index, length){
 					var succ = function(response){feeds.removeAt(index);feeds.sync();};
-					var fail = function(response){console.log(response)};
-					var content = item.data;
+					var fail = function(response){console.log(response);};
+					var content = item.data.data;
+					content.timestamp = item.data.timestamp;
 					console.log(item);
 					console.log(item.data);
 					//content.timestamp = item.timestamp;
-					HEART.toSensor(content, succ, fail);					
+					HEART.toSensocol(content, succ, fail);					
 				});
 		user = HEART.getItem('local', 'pnapi');
 		user =JSON.parse(user);
@@ -99,15 +108,15 @@ var HEART = {
 	audioPlay: 'strawberry',
 	audioCallback: {},
 	setAudio: function(name, succ, fail) {
-	  url=HEART.audioRoot+name;
-	  audio=new Media(url,succ,fail);
-	  HEART.audioPlay = audio;
+		url=HEART.audioRoot+name;
+		audio=new Media(url,succ,fail);
+		HEART.audioPlay = audio;
 	},
 	
 	getAudio: function() {
-	  return HEART.audioPlay;
+		return HEART.audioPlay;
 	},
 	
 	probability: 0.55
 	
-}
+};
